82219cce8792b894880e607eeba12898052f8eb6
parithosh <parithosh.jayanthi@ethereum.org>
2021-01-18 16:36:23 +0100

    updated crash checked with docker iamge

---
+++ scripts/crash-checker.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+read -p "Enter beaconfuzz_v2 folder with crashes (e.g: hfuzz_workspace/diff_attestation): " ARTIFACT_FOLDER
+
+echo "Enter container type- [possible values: Attestation, AttesterSlashing, Block, BlockHeader, Deposit, ProposerSlashing, VoluntaryExit]"
+read -p "input: " CONTAINER_TYPE
+
+. ./env
+
+echo "container type: $CONTAINER_TYPE"
+
+#BEACONFUZZ_V2_BASE="/tmp/fuzzing/beacon-fuzz/beaconfuzz_v2"
+BEACONFUZZ_V2_BASE="/Users/parithosh/dev/ansible/ansible-beaconfuzz-runner/fuzzing-artifacts"
+
+echo "Number of crash files to test: $(ls -l $BEACONFUZZ_V2_BASE/$ARTIFACT_FOLDER | grep -v ^l | wc -l) "
+NUMBER_OF_FALSE_POSITIVES=0
+NUMBER_OF_ACTUAL_CRASHES=0
+FAILURE_ARRAY=("")
+
+for filename in "$BEACONFUZZ_V2_BASE"/"$ARTIFACT_FOLDER"/*.fuzz; do
+  echo "testing $filename"
+  FALSE_COUNTER=0
+
+  FALSE_COUNTER=$( docker run -v "$BEACONFUZZ_V2_BASE"/"$ARTIFACT_FOLDER":"$BEACONFUZZ_V2_BASE"/"$ARTIFACT_FOLDER"  beaconfuzz_v2 ./beaconfuzz_v2 debug beacon.ssz "$filename" "$CONTAINER_TYPE" | grep -c "false")
+
+#  FALSE_COUNTER=$(./beaconfuzz_v2 debug beacon.ssz "$filename" "$CONTAINER_TYPE" | grep -c "false")
+  #./beaconfuzz_v2 debug beacon.ssz "$filename" "$CONTAINER_TYPE"
+  if [ "$(( FALSE_COUNTER % 2))" -ne 0 ]; then
+        echo "Only 1 false detected!"
+        NUMBER_OF_ACTUAL_CRASHES=$((NUMBER_OF_ACTUAL_CRASHES + 1))
+        FAILURE_ARRAY+=("$filename")
+  else
+        echo "Unable to recreate error"
+        NUMBER_OF_FALSE_POSITIVES=$((NUMBER_OF_FALSE_POSITIVES + 1))
+  fi
+done
+
+echo "********************************************"
+echo "Deleting unused docker containers (a ton would have been created by this script)"
+echo "********************************************"
+
+docker system prune
+
+echo "Number of false positives: $NUMBER_OF_FALSE_POSITIVES"
+echo "Number of actual recreated crashes: $NUMBER_OF_ACTUAL_CRASHES"
+echo "Please re-confirm that all the files are accounted for (add up the numbers)."
